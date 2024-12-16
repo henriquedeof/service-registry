13ff04397ab8942e3dd455e78cd86cbae9e3b1e0
henri <henriquedeof@gmail.com>
2024-12-16 15:08:27 +1100

    First commit

--- src/main/java/com/xpto/distancelearning/serviceregistry/ServiceRegistryApplication.java
+++ src/main/java/com/xpto/distancelearning/serviceregistry/ServiceRegistryApplication.java
@@ -2,12 +2,14 @@ package com.xpto.distancelearning.serviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
-	}
-
+    // Open http://localhost:8761/ in your browser to see the Eureka dashboard
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
 }
